e422b28bfcba5101d63ea8a3c0fab8b723be6bfa
lingzhi.fu <2835200509@qq.com>
2025-10-27 17:43:53 +0800

    fix:修改检查结果loading和字体

--- src/components/check/InspectionReport.vue
+++ src/components/check/InspectionReport.vue
@@ -640,7 +640,7 @@ onUnmounted(() => {
     position: absolute;
     top: 0;
     left: 0;
-    width: 100vw;
+    width: 100%;
     height: 100%;
     background: rgba(255, 255, 255, 0.7);
     z-index: 999;
